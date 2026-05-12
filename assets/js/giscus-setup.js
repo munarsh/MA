9ee4d782dc6d8585523a4462d25480d2a33309e0
munarsh <munarsh@clarkson.edu>
2026-05-12 17:47:03 +0000

    Deploying to gh-pages from @ munarsh/MA@86596c36bf0dee5256c214cfb0540d5cfe8fc65f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "munarsh/muneebarshad.github.io",
+    "data-repo": "munarsh/MA",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
