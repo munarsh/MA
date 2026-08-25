48b1bb2e6c976e4136d757cf10ab501eaad9b432
munarsh <munarsh@clarkson.edu>
2026-08-25 02:52:14 +0000

    Deploying to gh-pages from @ munarsh/MA@30abf2490b2c6d037a286b49d2cc9ab5ef6927a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/MA/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/MA/teaching/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
